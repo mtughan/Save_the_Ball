8148b94495e55a24975042b894ea84d8e29f8ed5
Joao Chavez <joao@translucentcomputing.com>
2014-03-24 09:46:15 -0400

    Added high score and options view controllers

---
+++ STBHighScoreViewController.m
@@ -0,0 +1,49 @@
+//
+//  STBHighScoreViewController.m
+//  SaveTheBall
+//
+//  Created by Ricardo Chavez on 2014-03-24.
+//  Copyright (c) 2014 Spiral Iris Software. All rights reserved.
+//
+
+#import "STBHighScoreViewController.h"
+
+@interface STBHighScoreViewController ()
+
+@end
+
+@implementation STBHighScoreViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
+{
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
---
+++ STBOptionsViewController.h
@@ -0,0 +1,13 @@
+//
+//  STBOptionsViewController.h
+//  SaveTheBall
+//
+//  Created by Ricardo Chavez on 2014-03-24.
+//  Copyright (c) 2014 Spiral Iris Software. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface STBOptionsViewController : UIViewController
+
+@end
