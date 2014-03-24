1074cb1788636c309471809883ddaf843d272188
kodywu7 <Kody@Kodys-MacBook-Pro.local>
2014-03-23 22:14:39 -0400

    Added Navigation Controller, Main Menu View, Social View Controller

---
+++ SaveTheBall/STBMainMenuViewController.h
@@ -0,0 +1,13 @@
+//
+//  STBMainMenuViewController.h
+//  SaveTheBall
+//
+//  Created by Kody Wu on 2014-03-23.
+//  Copyright (c) 2014 Spiral Iris Software. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface STBMainMenuViewController : UIViewController
+
+@end
