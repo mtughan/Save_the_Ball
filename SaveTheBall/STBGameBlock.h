975bcb0309b0c0b52622fdca6f81f2e948dbd567
Michael Tughan <mtughan@gmail.com>
2014-03-24 09:57:57 -0400

    Create new class STBGameBlock, and new group SpriteKit Classes for the game block and the scene.

---
+++ SaveTheBall/STBGameBlock.h
@@ -0,0 +1,13 @@
+//
+//  STBGameBlock.h
+//  SaveTheBall
+//
+//  Created by Michael Tughan on 2014-03-24.
+//  Copyright (c) 2014 Spiral Iris Software. All rights reserved.
+//
+
+#import <SpriteKit/SpriteKit.h>
+
+@interface STBGameBlock : SKShapeNode
+
+@end
---
+++ SaveTheBall/STBGameBlock.m
@@ -0,0 +1,13 @@
+//
+//  STBGameBlock.m
+//  SaveTheBall
+//
+//  Created by Michael Tughan on 2014-03-24.
+//  Copyright (c) 2014 Spiral Iris Software. All rights reserved.
+//
+
+#import "STBGameBlock.h"
+
+@implementation STBGameBlock
+
+@end
